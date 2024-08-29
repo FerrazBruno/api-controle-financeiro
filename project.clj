ab8d61bf06e98d68445534d2da9174ab213e9b2e
Bruno Ferraz <brunohenriqueferraz94@gmail.com>
2024-08-29 12:05:41 -0300

    adicionando descrição ao projeto

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject financeiro "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "API de Controle Financeiro"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
